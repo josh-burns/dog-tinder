df90eb728465a58b61e8a4115d46fb1d4062c253
josh-burns <josh.burns1993@gmail.com>
2020-11-14 17:25:27 +0000

    padding but no margin added to matched dog card

--- src/components/MatchedDogCard.js
+++ src/components/MatchedDogCard.js
@@ -7,8 +7,8 @@ const ImgStyle = styled.image`
   display: flex;
   margin-left: auto;
   margin-right: auto;
-  max-width: 50%;
-  max-height: 20%;
+  max-width: 30%;
+  max-height: 30%;
   border-radius: 70%;
   margin-left: auto;
   margin-right: auto;
@@ -20,7 +20,7 @@ const MatchedDogCardStyle = styled.div`
   height: 30%;
   max-height: 400px;
   display: block;
-  padding-bottom: 5%;
+  padding-bottom: 10%;
   box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
     0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
     0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
